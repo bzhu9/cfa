f1d7974f6133219b32555e4800e05c04c9808a23
Brian Zhu <zhu1013@purdue.edu>
2022-10-19 20:59:22 -0400

    testing adaptive mean thresholding

--- receipt_parser.py
+++ receipt_parser.py
@@ -10,6 +10,7 @@ norm_img = np.zeros((img.shape[0], img.shape[1]))
 img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
 img = cv2.threshold(img, 100, 200, cv2.THRESH_TRUNC)[1]
 # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+# img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
 img = cv2.normalize(img, norm_img, 0, 200, cv2.NORM_MINMAX)
 img = cv2.GaussianBlur(img, (1, 1), 0)
 kernel = np.ones((2, 1), np.uint8)
