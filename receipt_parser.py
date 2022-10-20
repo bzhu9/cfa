720d9418c9f44a82cc02a678921b0adbfead1400
Brian Zhu <zhu1013@purdue.edu>
2022-10-19 20:58:42 -0400

    testing adaptive gaussian thresholding

--- receipt_parser.py
+++ receipt_parser.py
@@ -9,6 +9,7 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 norm_img = np.zeros((img.shape[0], img.shape[1]))
 img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
 img = cv2.threshold(img, 100, 200, cv2.THRESH_TRUNC)[1]
+# img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 img = cv2.normalize(img, norm_img, 0, 200, cv2.NORM_MINMAX)
 img = cv2.GaussianBlur(img, (1, 1), 0)
 kernel = np.ones((2, 1), np.uint8)
